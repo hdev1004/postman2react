9f409b2be2555452ad20dabe9b328aa5ccbfb6c3
hdev1004 <h.dev@kakao.com>
2023-10-07 02:29:58 +0900

    FEAT: 버전 변경

--- src/App.js
+++ src/App.js
@@ -3,18 +3,8 @@ import './App.css';
 import Postman2React from './lib/postman2React';
 //import { Postman2React } from 'postman2react';
 import postmanJson from "./postman_collection.json";
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 function App() {
-  let [data, setData] = useState({});
-
-  useEffect(() => {
-    axios.get("https://raw.githubusercontent.com/hdev1004/postman2react/master/src/postman_collection.json").then((res) => {
-      setData(res.data);
-    })
-  }, [])
-
   return (
     <div>
         <Postman2React postmanJson={postmanJson} url={"*"} equalize={true}></Postman2React>
